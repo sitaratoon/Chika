6c114e774eca310f7dbf2827c0f417f642412a38
sitaratoon <ajay393798@gmail.com>
2025-02-09 13:24:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ getLogger("pyrogram").setLevel(WARNING)
 def LOGGER(name: str) -> Logger:
     return getLogger(name)
 
-if bool(environ.get("WEBHOOK", true)):
+if bool(environ.get("WEBHOOK", True)):
 
     try:
         API_ID = int(environ.get("API_ID", 18946488))
