16490cdc776c49b4c1f93ee50b77c8f1ad1b5f4f
sitaratoon <ajay393798@gmail.com>
2025-02-09 13:12:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ def LOGGER(name: str) -> Logger:
 if bool(environ.get("WEBHOOK", False)):
 
     try:
-        API_ID = int(environ.get("API_ID", "18946488"))
+        API_ID = int(environ.get("API_ID", "18946488))
     except Exception as e:
         print(f"API_ID Invalid: \n\nLogs: {e}")
 
